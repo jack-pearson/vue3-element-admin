7000b6144bb91f58ddba5ae3d67556ffefb6fd6b
jack-pearson <qize953463876@gmail.com>
2021-12-22 18:41:00 +0800

    修改

--- src/apis/login/index.ts
+++ src/apis/login/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: jack-pearson
  * @Date: 2021-11-23 16:41:53
- * @LastEditTime: 2021-12-14 14:14:33
+ * @LastEditTime: 2021-12-22 18:22:10
  * @LastEditors: jack-pearson
  * @FilePath: /yh-vue3-admin/src/apis/login/index.ts
  * @Description:
@@ -12,7 +12,7 @@ import { User } from "@/types/user";
 export default class LoginService {
   public static async login(data: any) {
     return request<User>({
-      url: "/web/login",
+      url: "/login/web",
       method: "post",
       data,
     });
