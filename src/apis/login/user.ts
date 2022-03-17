43a6e0b5af0cd80c0b4aa5db756cb7367e68269d
jack-pearson <qize953463876@gmail.com>
2022-03-17 17:51:31 +0800

    🚑 修改 token

--- src/apis/login/user.ts
+++ src/apis/login/user.ts
@@ -1,15 +1,14 @@
 /*
  * @Author: jack-pearson
  * @Date: 2022-03-17 17:37:21
- * @LastEditTime: 2022-03-17 17:38:33
+ * @LastEditTime: 2022-03-17 17:51:16
  * @LastEditors: jack-pearson
  * @FilePath: /vue3-element-admin/src/apis/login/user.ts
  * @Description:
  */
 export default {
   data: {
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDc1MTcwMjQsImRhdGEiOnsiaWQiOjEsImFjY291bnQiOiJhZG1pbiIsInBhc3N3b3JkIjoiMTIzNDU2IiwibmFtZSI6IueuoeeQhuWRmCIsImVtYWlsIjoicWl6ZTk1MzQ2Mzg3NkBnbWFpbC5jb20iLCJtb2JpbGUiOiIxNzY4MzcwODM3MSIsInBob25lIjoiODU3NjI4OCIsImFnZSI6MzIsImF2YXRhciI6bnVsbCwic2V4IjoibnVsbCIsImNyZWF0ZVRpbWUiOiIyMDIxLTEyLTI3VDIyOjU1OjI1LjAwMFoiLCJ1cGRhdGVUaW1lIjoiMjAyMi0wMS0xOFQyMjozNDoyMi4yMzdaIn0sImlhdCI6MTY0NzUwOTgyNH0.QlwWDH76c-4csDGlU93ysyzA-3fS7oGMV4xVFkNcdaQ",
+    token: "test",
     id: 1,
     account: "admin",
     password: "123456",
