f0d25623c8707e4a60b9d13a96301f48e8ce47ec
jack-pearson <qize953463876@gmail.com>
2022-03-17 15:35:24 +0800

    增加 husky

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,16 +1,16 @@
 <!--
  * @Author: jack-pearson
  * @Date: 2022-01-11 13:33:47
- * @LastEditTime: 2022-03-17 15:31:34
+ * @LastEditTime: 2022-03-17 15:35:10
  * @LastEditors: jack-pearson
  * @FilePath: /vue3-element-admin/src/views/profile/index.vue
  * @Description: 
 -->
 <template>
-  <div>1235</div>
+  <div class="profile">profile</div>
 </template>
 <script setup lang="ts"></script>
-<style lang="scss" scoped>
-.profile {
-}
-</style>
+<style lang="scss" scoped></style>
+<script lang="ts">
+export default { name: "profile" };
+</script>
